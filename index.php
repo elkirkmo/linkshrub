7687a75f844d67bf1c0072a111bd414d10b9db4b
Chris Kirkham <me@chriskirkham.com>
2019-11-18 00:08:21 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Link_Shrub
+ * @version 0.1.0
+ */
+/*
+Plugin Name: linkshrub
+Plugin URI:
+Description: A custom link page for easy sharing
+Author: Chris Kirkham
+Version: 0.1.0
+Author URI: https://chriskirkham.com/
+*/
